357aae62c2add8e7d26a2ae45b6bfb1463cdf27f
Using git <dangsangnguyen260399@gmail.com>
2020-06-27 10:41:57 +0700

    add method eat cat

--- Cat.js
+++ Cat.js
@@ -1,5 +1,15 @@
+var Mouse = require('Mouse');
+
 function Cats() {
 	this.stomach = [];
+}
+
+Cats.prototype.eat = function(animal){
+	if (animal instanceof Mouse){
+		this.stomach.push(animal);
+	}else {
+		console.log('Cat can only mouse!');
+	}
 }
 
 module.exports = Cats;
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var Dog = require('./Dog');
 var Cat = require('./Cat');
+var Mouse = require('./Mouse');
 
 var dog = new Dog('Bilu');
 var tom = new Cat();
+var Micky = new Mouse();
 
-dog.eat(tom);
-console.log(dog);
-dog.sayHi();
+// dog.eat(tom);
+// console.log(dog);
+// dog.sayHi();
+
+tom.eat(Micky);
+console.log(tom);
